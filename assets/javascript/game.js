721232862aa17a036fb18fd6d65bfe73a39c7963
Lucas Di Fini <difinile@gmail.com>
2019-09-25 20:58:47 -0600

    index and script updated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,95 +1,68 @@
-function getRandomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
+var won = 0;
+var lost = 0;
+var randomNumber;
+var counter = 0;
+var crystal1;
+var crystal2;
+var crystal3;
+var crystal4;
+var resetGame = false;
+
+ranNumber();
+$("#number").text(randomNumber);
+var imageCrystal = $(".crystal");
+//for loop
+// for (var i = 0; i < numberOptions.length; i++) {
+//     imageCrystal.attr("data-crystalvalue", numberOptions[i]);
+
+// }
+
+function reset() {
+    ranNumber()
+    counter = 0;
+    $("#number").text(randomNumber);
+    $("#score").text(counter);
+    resetGame = true;
+
 }
+$(".crystal").on("click", function () {
+    if (resetGame) {
+        $("#winLose").empty();
+        resetGame = false;
+    }    
+    var crystalValue = $(this).attr("id");
+    if (crystalValue === "blue") {
+        counter = counter + crystal1
+    } else if (crystalValue === "green") {
+        counter = counter + crystal2
+    } else if (crystalValue === "purple") {
+        counter = counter + crystal3
+    } else if (crystalValue === "red") {
+        counter = counter + crystal4
+    }
 
-function startGame() {
-    var gameSettings = {
-        currentSum: 0,
-        targetRandNumber: getRandomNumber(19, 120)
-        }
-    return gameSettings;
+
+    $("#score").text(counter);
+
+    if (counter === randomNumber) {
+        reset();
+        won ++;
+        $("#wins").text(won);
+        $("#winLose").text("You win!")
+    }
+
+    else if (counter > randomNumber) {
+        reset();
+        lost ++;
+        $("#lost").text(lost);
+        $("#winLose").text("You loose!")
+    }
+
+});
+function ranNumber() {
+    crystal1 = Math.floor(Math.random() * 9) + 1;
+    crystal2 = Math.floor(Math.random() * 9) + 1;
+    crystal3 = Math.floor(Math.random() * 9) + 1;
+    crystal4 = Math.floor(Math.random() * 9) + 1;
+    randomNumber = Math.floor(Math.random() * ((120 - 19) + 1) + 19)
 }
-
-function setJewelValue (item) {
-    item.attr("data-crystalvalue", getRandomNumber(1,12));
-}
-
-function userTotal (stats, sum){
-    stats.currentSum += sum;
-    return stats.currentSum;
-}
-
-function animateImage (item, index, arr) {
-    arr[0].css({"top":"12rem","left":"2rem"});
-    arr[1].css({"top":"12rem","left":"15rem"});
-    arr[2].css({"top":"12rem","right":"15rem"});
-    arr[3].css({"top":"12rem","right":"2rem"});
-    $(".crystal-image").animate({
-        height: '+=300px',
-        width: '+=300px'
-    },"slow");
-    $(".crystal-image").animate({
-        height: '-=300px',
-        width: '-=300px'
-    },"slow");
-}
-
-function resetPosition() {
-    setTimeout(function(){
-        $(".crystal-image").removeClass("imgAbsolute");
-    }, 5000);
-}
-
-function updateScore(item, value) {
-    item.text(value);
-}
-
-function newGame (jewelsArr) {
-    stats = startGame();
-    $("#target-Num").text(stats.targetRandNumber);
-    $("#current-Sum").text(stats.currentSum);
-    jewelsArr.forEach(setJewelValue);
-    return stats;
-}
-
-$(document).ready(function () {
-
-    var wins = 0;
-    var losses = 0;
-
-    var winsItem = $("#wins").text(wins);
-    var lossItem = $("#losses").text(losses); 
-
-    var blue = $("#blue");
-    var green = $("#green");
-    var purple = $("#purple");
-    var red = $("#red");
-
-    var jewels = [blue,green,purple,red];
-    jewels.forEach(setJewelValue);
-
-    var gameStats = newGame(jewels);
-  
-    $(".crystal-image").on("click", function() {
-        crystalValue = ($(this).attr("data-crystalvalue"));
-        crystalValue = parseInt(crystalValue);
-        $("#current-Sum").text(userTotal(gameStats, crystalValue));
-    
-        if (gameStats.currentSum === gameStats.targetRandNumber) {
-            alert("You win!");
-            $(".crystal-image").addClass("imgAbsolute")
-            jewels.forEach(animateImage);
-            resetPosition();
-            wins++;
-            updateScore(winsItem,wins);
-            //winsItem.text(wins);
-            gameStats = newGame(jewels);
-        } else if (gameStats.currentSum >= gameStats.targetRandNumber) {
-            alert("You lose!!");
-            losses++;
-            updateScore(lossItem,losses);
-            //lossItem.text(losses);
-            gameStats = newGame(jewels);
-        }
-    });
-}); 
